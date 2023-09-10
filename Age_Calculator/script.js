25f2edf801d16237fef0dee61b26b0e75408d596
yogendra09 <yogendrayadav168@gmail.com>
2023-09-10 22:44:52 +0530

    Age_calculator

---
+++ Age_Calculator/script.js
@@ -0,0 +1,49 @@
+let userinpt = document.querySelector('#date');
+
+userinpt.max = new Date().toISOString().split("T")[0];
+
+
+function calculateAge(){
+    let birthDate = new Date(userinpt.value);
+    let d1 = birthDate.getDate() 
+    let m1 = birthDate.getMonth() +1 
+    let y1 = birthDate.getFullYear() 
+
+    let today = new Date();
+    let d2 = today.getDate()
+    let m2 = today.getMonth() + 1
+    let y2 = today.getFullYear()
+
+    let d3 , m3 , y3;
+
+    y3= y2-y1;
+
+    if(m2>=m1){
+        m3 = m2-m1;
+    } 
+    else{
+        y3--;
+        m3= 12 + m2- m1
+    }
+
+    if(d2 >= d1){
+        d3 = d2-d1;
+    }
+    else{
+          m3--;
+          d3=getDays(y1,m1) + d2-d1;
+    }
+    if(m3<0){
+        m3=11;
+        y3--;
+    }
+    console.log(y3);
+   $("#result").html(`you are ${y3} year, ${m3} month, ${d3} days`)
+}
+
+
+function getDays(year,month){
+    return new Date(year,month,0).getDate();
+
+
+}
